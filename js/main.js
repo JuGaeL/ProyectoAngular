b514f44e65f671879fc93ad6ccebf7eb5538f6ff
Mario Mora <marioemorac@gmail.com>
2022-10-11 00:39:02 -0600

    Búsqueda agregada
    Se agregaron búsqueda y efecto de opacidad

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 var app = angular.module('myApp', []);
 
+app.controller('CtrlTop', function( $scope, busqueda ){
+  $scope.$watch('busqueda', function(newValue){
+    $scope.$emit('busquedaCambio', newValue);
+  });
+});
+
 app.controller('comics', function ($scope, $http) {
     $http.get('https://gateway.marvel.com/v1/public/events?ts=1&apikey=cd4582fb1127b00689682fbc261d7438&hash=6e240ec391d5cd18df34b969266c84c8')
         .then((response) => {
             $scope.items = response.data.data.results;
-        })
-})
+        });
+
+        $scope.$on('busquedaCambio', function(event, value){
+          $scope.busqueda = value;
+        });
+});
+
+
